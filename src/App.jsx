b5d82b0f006c8f8592741dfcbc1640b47e442660
MohamedDharik <mohameddharik.h.2021.ece@ritchennai.edu.in>
2024-07-24 18:14:06 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from './Header/header.jsx'
+import SignUp from './Signup/Signup.jsx'
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+function App() {
+
+   return(
+    <>
+    <Header/>
+    <SignUp/>
+    </>
+   );
+}
+
+export default App
